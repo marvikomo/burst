534e12a739a2cf885f481566fb3c8232201a37f0
marvikomo <marvikomo@gmail.com>
2022-06-07 00:12:44 +0100

    created genesis block

--- src/Block.rs
+++ src/Block.rs
@@ -1,8 +1,21 @@
+use chrono::prelude::*;
 pub struct Block {
     id: u64,
     timestamp: i64,
     hash: String,
-    previous_hash: &str,
-    Validator: &str
+    previous_hash: String,
+    //transaction: vec [],
+    Validator: String,
+}
 
+impl Block {
+    pub fn genesis() -> Self {
+        Block {
+            id: 1,
+            timestamp: Utc::now().timestamp(),
+            hash: String::from("0"),
+            previous_hash: String::from("0"),
+            Validator: String::from("0"),
+        }
+    }
 }
