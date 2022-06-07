4f72004c4c9f170e0ab35803b96c7ab47f798cab
marvikomo <marvikomo@gmail.com>
2022-06-07 18:43:52 +0100

    implemneted transaction struct

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,3 +1,16 @@
+
 pub struct Transaction{
-    
+    transaction_id: String,
+    transaction_timestamp: i64,
+    transaction_details: String
+}
+
+impl Transaction {
+    pub fn new(transaction_id, transaction_timestamp, transaction_details) -> Self{
+        Self{
+            transaction_id,
+            transaction_timestamp,
+            transaction_details
+        }
+    }
 }
