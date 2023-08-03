3b218b2e450629a302f9f25db0abaaa8fe8949db
marvikomo <marvikomo@gmail.com>
2023-08-04 00:48:46 +0100

    implemented bosh serialize hash

--- src/blockchain/hash.rs
+++ src/blockchain/hash.rs
@@ -1,8 +1,12 @@
-use sha2::Digest;
+use sha2::{Digest, Sha256};
 use borsh::{BorshSerialize, BorshDeserialize};
 
 pub struct CryptoHash(pub [u8; 32]);
 
+#[derive(
+borsh::BorshDeserialize,
+borsh::BorshSerialize,
+)]
 impl CryptoHash {
 
     pub const fn new() -> Self {
@@ -13,6 +17,15 @@ impl CryptoHash {
         CryptoHash(sha2::Sha256::digest(bytes).into())
      }
 
+    pub fn get_hasher() ->  Sha256 {
+        sha2::Sha256::default()
+    }
+
+    pub fn bosh_serialize_hash<T: BorshSerialize>(value: T) -> CryptoHash {
+        let mut hasher = get_hasher();
+        value.serialize(&mut hasher).unwrap();
+        CryptoHash(hasher.finalize().into())
+    }
 
 
 }
