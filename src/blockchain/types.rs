787983f55946b3600da1c754e755729644685cd9
marvikomo <marvikomo@gmail.com>
2023-08-04 13:17:04 +0100

    added types

---
+++ src/blockchain/types.rs
@@ -0,0 +1,8 @@
+pub type Validatorid = u64;
+pub type Nonce = u64;
+pub type BlockHeight = u64;
+pub type EpochHeight = u64;
+pub type Balance = u128;
+pub type Gas = u64;
+pub type BlockNum = u64;
+pub type ProtocolVersion = u32;
