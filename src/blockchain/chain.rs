2c42ef166dfa09f5786d1f17dbbbc3f30b0c2c52
marvikomo <marvikomo@gmail.com>
2023-10-05 18:25:05 +0100

    modified the block and block header structure

--- src/blockchain/chain.rs
+++ src/blockchain/chain.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)]  //this turns of warning for vars I don't use
 #![allow(unused_imports)]
 
-use super::transaction::Transaction;
+//use super::transaction::Transaction;
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -11,27 +11,71 @@ use crate::blockchain::hash::CryptoHash;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BlockHeader {
-    pub height: u64,
-    pub hash: CryptoHash,
-    pub previous_hash: CryptoHash,
+    pub vesrion: Version,
+    pub chain_id: String, //identifier od the blockchain
+    pub height: u64, //the height of the block
+    pub last_block_id: BlockID, //reference to the previous block, this ensures blocks are added in sequence and allows for entire chain to be verified from genesis to the latest block
+    pub last_commit_hash:[u8; 32] , //Hash of the previous commit
+    pub data_hash: [u8; 32], //Hash of the transaction in the block
+    pub validators_hash:[u8; 32],  //Hash of the validator set for the current block
+    pub next_validators_hash: [u8; 32],
+    pub consensus_hash: [u8; 32], //consensus parameters might change overtime (e.g due to governance decisions). Storing a hash of these parameters ensures that all validators can participate in the consensus process
+    pub app_hash: [u8; 32], //This allows tendermint to be agnostic to the application logic. It ensures that all validators' state machine process transactions consistently
+    pub last_result_hash: [u8; 32],
+    pub evidence_hash: [u8; 32], //To store evidence of any byzantine behaviour by validators. Storing evidence of malicious behavior ensures that malicious validators can be penalized
+    pub proposer_address: [u8; 32], //This indicates which proposer proposed the block
     pub epoch_id: EpochId,
     pub next_epoch_id: EpochId,
-    pub gas_price: Balance,
+    pub gas_price: u68,
     pub validator_reward: Balance,
     pub timestamp: u64,
-    pub signature: Signature,
     pub protocol_version: ProtocolVersion
 }
+
+struct Version {
+    block: i64,
+    app: i64
+}
+
+struct BlockID {
+  hash: [u8; 32],
+  parts_header: PartsHeader,
+}
+
+struct PartsHeader {
+ total: i32,
+ hash: [u8; 32]
+}
+
+
 
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
     id: u64,
+    block_header: BlockHeader,
     timestamp: i64,
-    hash: Option<String>,
-    previous_hash: Option<String>,
-    validator: Option<String>,
-    block_transactions: Vec<Transaction>,
+    data: Data,
+    evidence: Evidence,
+    last_commit: LastCommit
+}
+
+struct Data {
+    txs: Vec<Transaction>
+}
+
+struct Evidence {
+    evidence: Vec<EnvidenceDetails>
+}
+//the structure of the transaction will depend on what BURST requires, we will look more into this but for now lets keep it simple but
+//however, a transaction is often treated as a raw byte array since Tendermint is agnostic to the transaction's content
+struct Transaction {
+
+}
+
+//Also lets look into what this will look like later
+struct EnvidenceDetails {
+
 }
 
 impl Block {
@@ -40,10 +84,6 @@ impl Block {
         Self {
             id,
             timestamp: Utc::now().timestamp(),
-            hash: None,
-            previous_hash: Some(String::from("00x")),
-            validator: Some(String::from("00x")),
-            block_transactions:vec![]
         }
     }
     pub fn genesis() -> Self {
@@ -55,10 +95,7 @@ impl Block {
         Block {
             id: 1,
             timestamp: Utc::now().timestamp(),
-            hash: Some(String::from("0")),
-            previous_hash: Some(String::from("0")),
-            validator: Some(String::from("0")),
-            block_transactions: vec![transaction]
+
         }
     }
     pub fn generate_hash(&self) -> String {
--- src/blockchain/hash.rs
+++ src/blockchain/hash.rs
@@ -10,18 +10,6 @@ borsh::BorshSerialize,
 )]
 impl CryptoHash {
 
-    impl Default for CryptoHash {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
-
-    impl serde::Serialize for CryptoHash {
-        fn serialize<S>(&self, serializer: S) -> Result<serde::ser::Ok, serde::ser::Error> where S: Serializer {
-
-        }
-    }
-
     fn to_base58_impl(encoded: &str) -> Decode58Result {
         let mut result = Self::new();
         match bs58::decode(encoded).into(&mut result.0) {
@@ -30,7 +18,7 @@ impl CryptoHash {
             Err(err) => Decode58Result::Err(err),
         }
     }
-    }
+
 
     pub const fn new() -> Self {
         Self([0; Self::LENGTH])
@@ -51,6 +39,17 @@ impl CryptoHash {
     }
 
 
+}
+impl Default for CryptoHash {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl serde::Serialize for CryptoHash {
+    fn serialize<S>(&self, serializer: S) -> Result<serde::ser::Ok, serde::ser::Error> where S: Serializer {
+
+    }
 }
 
 
